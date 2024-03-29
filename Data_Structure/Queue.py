a9de5d8e5b1b08d92ebfdea6ae42ba8a0c3ec1ed
Huzaifa <huzaifanaeem909@gmail.com>
2024-03-29 19:57:00 +0500

    Added Queue Overview

---
+++ Data_Structure/Queue.py
@@ -0,0 +1,43 @@
+""" A queue is a useful data structure in programming. It is similar to the ticket queue outside a cinema hall, 
+where the first person entering the queue is the first person who gets the ticket.
+Queue follows the First In First Out (FIFO) rule - the item that goes in first is the item that comes out first. """
+
+# Queue implementation in Python
+
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    # Add an element
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    # Remove an element
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        return self.queue.pop(0)
+
+    # Display  the queue
+    def display(self):
+        print(self.queue)
+
+    def size(self):
+        return len(self.queue)
+
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+
+q.display()
+
+q.dequeue()
+
+print("After removing an element")
+q.display()
