3eacab90890589786b3a9df554012f1990103a65
Huzaifa <huzaifanaeem909@gmail.com>
2024-08-13 22:18:59 +0500

    Function to find the number closest to zero

--- Problems/Arrays & Strings/closest_to_zero.py
+++ Problems/Arrays & Strings/closest_to_zero.py
@@ -18,6 +18,25 @@ def findClosestNumber(arr):
         # If two numbers are equally close to zero, prefer the positive one
         elif abs(i) == abs(closest_number) and i > closest_number:
             closest_number = i
+    
+    # Return the closest number found
+    return closest_number
+
+# Example usage
+arr = [-4, -2, -1, 1, 4, 8]
+print("Closest number to Zero:", findClosestNumber(arr))
+
+# Another Solution:
+
+def findClosestNumber(arr):
+    # Initialize closest_number with the first element of the array
+    closest_number = arr[0]
+    
+    # Iterate through the array to find the number closest to zero
+    for i in arr:
+        # Update closest_number if the current number is closer to zero
+        if abs(i) < abs(closest_number):
+            closest_number = i
 
     # Return the absolute value if the closest number is negative and its absolute value is also present in the array
     if closest_number < 0 and abs(closest_number) in arr:
@@ -27,5 +46,5 @@ def findClosestNumber(arr):
     return closest_number
 
 # Example usage
-arr = [-4, -2, -1, 1, 4, 8]
+arr = [-34, -22, -8, 10, 12, 8]
 print("Closest number to Zero:", findClosestNumber(arr))
