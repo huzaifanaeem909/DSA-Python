416d9dabfee02c765ebe9cbb5d3a9fbd99ad4687
Huzaifa <huzaifanaeem909@gmail.com>
2024-10-30 20:10:24 +0500

    Sort Array

---
+++ Problems/Arrays & Strings/Easy/Sort_0s,1s_and_2s.py
@@ -0,0 +1,39 @@
+"""
+Given an array arr containing only 0s, 1s, and 2s. Sort the array in ascending order.
+
+Examples:
+
+Input: arr[] = [0, 1, 2, 0, 1, 2]
+Output: [0, 0, 1, 1, 2, 2]
+Explanation: 0s 1s and 2s are segregated into ascending order.
+
+
+Input: arr[] = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+"""
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+
+def sort_array(arr):
+    low = 0
+    mid = 0
+    high = len(arr) - 1
+
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[high], arr[mid] = arr[mid], arr[high]
+            high -= 1
+    return arr
+
+
+arr = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+sorted_arr = sort_array(arr)
+print(sorted_arr)
